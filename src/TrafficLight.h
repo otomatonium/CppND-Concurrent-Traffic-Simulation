c26036244404df25cef891c3ea8880c8a7263f24
otomatonium <6641160+otomatonium@users.noreply.github.com>
2020-01-08 18:06:00 -0800

    Complete FP.1

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -30,17 +30,27 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
+class TrafficLight : TrafficObject
 {
 public:
+    enum TrafficLightPhase
+    {
+        red = 0,
+        green
+    };
+
     // constructor / desctructor
 
     // getters / setters
+    TrafficLightPhase getCurrentPhase() { return _currentPhase; }
 
     // typical behaviour methods
+    void waitForGreen();
+    void simulate();
 
 private:
     // typical behaviour methods
+    void cycleThroughPhases();
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
@@ -48,6 +58,7 @@ private:
 
     std::condition_variable _condition;
     std::mutex _mutex;
+    TrafficLightPhase _currentPhase;
 };
 
 #endif
